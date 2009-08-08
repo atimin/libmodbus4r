dbb9ed235e311b0d1c0a27b0844c7e58157bf979
Aleksey Timin <atimin@gmail.com>
2009-08-08 22:28:35 +0600

    New test for TCPSlave::coil_status

--- spec/tcp_slave_spec.rb
+++ spec/tcp_slave_spec.rb
@@ -3,29 +3,47 @@ require 'timeout'
 
 describe ModBus::TCPSlave do
   
-  before(:all) do
+  before(:each) do
     @sl = ModBus::TCPSlave.new('127.0.0.1', 1502, 1)
     @sl.start
-  end
-
-  it "should not stoped" do
-    @sl.stoped?.should == false
   end
 
   it "should have stoped" do
     sl = ModBus::TCPSlave.new('127.0.0.1', 1512, 1)
     sl.start.stoped? == false
     sl.stop.stoped? == true
-#    sl.start.stoped? == false
+    sl.start.stoped? == false
   end
 
   it "should open listen socket" do
     mstr = ModBus::TCPMaster.new('127.0.0.1', 1502, 1)
     mstr.connect.closed?.should == false
     mstr.close
+
+    @sl.stop
+    lambda { mstr.connect }.should raise_error(ModBus::Errors::ModBusError)
+
   end
 
-  after(:all) do
+
+  it "should have coil status" do
+    mstr = ModBus::TCPMaster.new('127.0.0.1', 1502, 1)
+    mstr.connect
+
+    @sl.coil_status.should == []
+    @sl.coild_status = [false, true, false]
+    
+    mstr.read_coil_status(0, 3).should == [false, true, false]
+    mstr.force_single_coil(1, false)
+     
+    @sl.coil_status.should == [false, false ,false]
+     
+    mstr.force_multiple_registers(0, [true, true, true])
+
+    @sl.coil_status.should == [true, true, true]
+  end
+
+  after(:each) do
     @sl.stop unless @sl.stoped?
   end
 
